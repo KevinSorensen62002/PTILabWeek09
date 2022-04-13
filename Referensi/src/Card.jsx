b27f512cfddb7ad3d94dd180d9a44e722f26721d
KevinSorensen62002 <101918443+KevinSorensen62002@users.noreply.github.com>
2022-04-13 09:05:37 +0700

    Add files via upload

---
+++ Referensi/src/Card.jsx
@@ -0,0 +1,23 @@
+import "../../public/styles.css";
+
+function Card(props) {
+  return (
+    <div className="box">
+      <div className="border"></div>
+      <div className="top">
+        <div className="name">
+          <h2>{props.name}</h2>
+          <div className="phone">
+            <p>{props.phone}</p>
+            <p>{props.email}</p>
+          </div>
+        </div>
+        <div className="gambar">
+          <img src={`${props.img}`} alt=""></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
